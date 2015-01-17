49aa1f17f5bcaeb2ccaa2a6742c8d5618b8b4a6b
Chris Scott <cyberstrike@gmail.com>
2015-01-17 18:28:39 -0500

    Whitelisted the Event#Calender from authentication

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :calendar]
   before_action :set_event, only: [:show, :edit, :update, :destroy]
   before_action :set_date, only: [:index, :calendar]
 
