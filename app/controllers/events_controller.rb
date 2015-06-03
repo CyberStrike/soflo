463ede73e09d3d64cca6b22f775a73fcc5533b61
Chris Scott <cyberstrike@gmail.com>
2015-06-03 17:22:51 -0400

    colors try 2

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -30,6 +30,10 @@ class EventsController < ApplicationController
   # GET /events/1
   # GET /events/1.json
   def show
+    @colors = %w(#f44336 #E91E63 #9C27B0 #673AB7
+                 #3F51B5 #2196F3 #03A9F4 #00BCD4
+                 #009688 #4CAF50 #8BC34A #CDDC39
+                 #FFEB3B #FFC107 #FF9800 #FF5722)
   end
 
   # GET /events/new
