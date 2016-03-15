9e5b224af15848df58fb1d6a89ac78b8d9cd7aed
Chris Scott <cyberstrike@gmail.com>
2016-03-15 16:20:20 -0400

    removed yellow from color stream

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -113,7 +113,7 @@ class EventsController < ApplicationController
       @colors = %w(#f44336 #E91E63 #9C27B0 #673AB7
                  #3F51B5 #2196F3 #03A9F4 #00BCD4
                  #009688 #4CAF50 #8BC34A #CDDC39
-                 #FFEB3B #FFC107 #FF9800 #FF5722)
+                 #FFC107 #FF9800 #FF5722)
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
